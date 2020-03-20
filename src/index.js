fa2d7bad1be53cd6ead630f94f1e4d54d341fe44
JennyOh1004 <ohseulki1004@gmail.com>
2020-03-19 19:56:48 -0700

    soobing commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <h1>Hello World! soobing branch</h1>,
+  document.getElementById("root")
+);
