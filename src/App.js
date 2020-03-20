f417b39bd7e54b37f778fa9e9db60ef55187c577
JennyOh1004 <ohseulki1004@gmail.com>
2020-03-19 20:09:47 -0700

    redux default setting

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as types from "./actions";
+
+export default function App() {
+  const dispatch = useDispatch();
+  const isTest = useSelector(state => state.app.isTest);
+  console.log(isTest);
+  return (
+    <h1
+      onClick={() => dispatch({ type: types.APP_ACTION_TEST, data: !isTest })}
+    >
+      Hello Redux World! => {String(isTest)}
+    </h1>
+  );
+}
